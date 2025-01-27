08375bbb0ac3845e7e54cf646df6f5ee36f995b9
Beslan <beslandudaev8@gmail.com>
2025-01-27 12:13:22 +0300

    local

--- src/todo.jsx
+++ src/todo.jsx
@@ -36,6 +36,7 @@ export function ToDoList() {
         localStorage.setItem('toDoTasks', JSON.stringify(tasklist))
     }, [tasklist])
 
+    
     return (
         <div className = 'container'>
             <header>
